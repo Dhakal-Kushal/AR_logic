ff208528502a053b7ecae59328bdfd078d01d4ef
Kushal_ <dhkushaldh@gmail.com>
2026-01-02 16:27:23 -0500

    obj rendering

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 img = cv2.imread('scene.jpg', 0)
 
@@ -65,3 +66,77 @@ if len(matches) > MIN_MATCHES:
         cv2.waitKey(0)
 else:
     print(f"Not enough matches have been found -{len(matches)}/{MIN_MATCHES}")
+
+
+def projection_matrix(camera_parameters, homography):
+    """
+    From the camera calibration matrix and the estimated homography
+    compute the 3D projection matrix
+    """
+    # Compute rotation along the x and y axis as well as the translation
+    homography = homography * (-1)
+    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
+    col_1 = rot_and_transl[:, 0]
+    col_2 = rot_and_transl[:, 1]
+    col_3 = rot_and_transl[:, 2]
+    # normalise vectors
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rot_1 = col_1 / l
+    rot_2 = col_2 / l
+    translation = col_3 / l
+    # compute the orthonormal basis
+    c = rot_1 + rot_2
+    p = np.cross(rot_1, rot_2)
+    d = np.cross(c, p)
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_3 = np.cross(rot_1, rot_2)
+    # finally, compute the 3D projection matrix from the model to the current frame
+    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+    return np.dot(camera_parameters, projection)
+
+def hex_to_rgb(hex_color):
+    """
+    Convert a hex color string to an (R, G, B) tuple.
+
+    Accepts formats:
+    - "#RRGGBB"
+    - "RRGGBB"
+    """
+    if isinstance(hex_color, str):
+        hex_color = hex_color.lstrip('#')
+    else:
+        raise TypeError("hex_color must be a string")
+
+    if len(hex_color) != 6:
+        raise ValueError("hex_color must be in RRGGBB format")
+
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+
+    return (r, g, b)
+
+
+def render(img, obj, projection, model, color=False):
+    vertices = obj.vertices
+    scale_matrix = np.eye(3) * 3
+    h, w = model.shape
+
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+        points = np.dot(points, scale_matrix)
+        # render model in the middle of the reference surface. To do so,
+        # model points must be displaced
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1] # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
